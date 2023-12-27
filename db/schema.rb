a0544c44ef5baf9db04e75432fb394ae2795f6fd
delasthanos <delasthanos@gmail.com>
2023-12-27 18:22:35 +0200

    Update database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_09_193417) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_27_155838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "aircraft", force: :cascade do |t|
+    t.string "model"
+    t.string "featured_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "user_groups", force: :cascade do |t|
     t.string "group"
@@ -22,28 +29,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_09_193417) do
     t.index ["group"], name: "index_user_groups_on_group", unique: true
   end
 
-  #
-  # Default Hardcoded User Groups
-  #
-  [
-    {
-      id: 100,
-      group: 'admin'
-    },
-    {
-      id: 200,
-      group: 'user'
-    },
-    {
-      id: 300,
-      group: 'guest'
-    }
-  ].each do |group|
-    UserGroup.find_or_create_by!(id: group[:id], group: group[:group])
-  end
-
   create_table "users", force: :cascade do |t|
-    t.references :user_group, foreign_key: true
+    t.bigint "user_group_id"
     t.string "email"
     t.string "username"
     t.string "password_digest"
@@ -52,44 +39,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_09_193417) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_email_on_users", unique: true
+    t.index ["user_group_id"], name: "index_users_on_user_group_id"
     t.index ["username"], name: "index_username_on_users", unique: true
   end
 
-  create_table "opensky_network_raw", force: :cascade do |t|
-    t.string "icao24"
-    t.string "registration"
-    t.string "manufacturericao"
-    t.string "manufacturername"
-    t.string "model"
-    t.string "typecode"
-    t.string "serialnumber"
-    t.string "linenumber"
-    t.string "icaoaircrafttype"
-    t.string "operator"
-    t.string "operatorcallsign"
-    t.string "operatoricao"
-    t.string "operatoriata"
-    t.string "owner"
-    t.string "testreg"
-    t.string "registered"
-    t.string "reguntil"
-    t.string "status"
-    t.string "built"
-    t.string "firstflightdate"
-    t.string "seatconfiguration"
-    t.string "engines"
-    t.string "modes"
-    t.string "adsb"
-    t.string "acars"
-    t.string "notes"
-    t.string "categoryDescription"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "aircraft", force: :cascade do |t|
-    t.string "model"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  add_foreign_key "users", "user_groups"
 end
