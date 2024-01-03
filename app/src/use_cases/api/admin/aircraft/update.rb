b3f5cdb199b6e00c12b8c48446f8a3c1d2177623
delasthanos <delasthanos@gmail.com>
2024-01-03 21:15:42 +0200

    Update aircraft details

---
+++ app/src/use_cases/api/admin/aircraft/update.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+module UseCases
+  module API
+    module Admin
+      module Aircraft
+        class Update < ::UseCases::API::Base
+          attr_reader :errors, :params
+
+          def initialize(params:)
+            super()
+
+            @params = params
+
+            @errors = []
+          end
+
+          def dispatch(&response) # rubocop:disable Metrics/MethodLength
+            if !verify_aircraft_exists?
+              @http_code = 422
+              add_error(code: :not_found, message: 'Could not find aircraft')
+
+              return error(&response)
+            end
+
+            if update_aircraft_details?
+              @http_code = 200
+              @message = 'Sucessfully updated aircraft'
+              @response_data = ::Aircraft.where(id: @aircraft.id)
+
+              return success(&response)
+            end
+
+            @http_code = 422
+            add_error(code: :failed, message: 'Could not update aircraft')
+
+            error(&response)
+          end
+
+          private
+
+          def existing_images
+            return @existing_images if instance_variable_defined?(:@existing_images)
+
+            @existing_images = ::AircraftImage.where(aircraft_id: @aircraft_id)
+          end
+
+          def image_urls_to_delete
+            return @image_urls_to_delete if instance_variable_defined?(:@image_urls_to_delete)
+            return [] if existing_images.blank?
+
+            @image_urls_to_delete = []
+
+            existing_images_urls = existing_images.map(&:url)
+            provided_images_urls = @images.map { |image| image['url'] }
+
+            existing_images_urls.each do |existing_image_url|
+              @image_urls_to_delete.push(existing_image_url) unless provided_images_urls.include?(existing_image_url)
+            end
+
+            @image_urls_to_delete
+          end
+
+          def update_aircraft_details?
+            @aircraft.model = params[:model] if params[:model]
+            @aircraft.description = params[:description] if params[:description]
+
+            @aircraft.save!
+          end
+
+          def verify_aircraft_exists?
+            @aircraft = ::Aircraft.find_by(id: params[:id])
+
+            return false if @aircraft.nil?
+
+            true
+          end
+        end
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_29_153050) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_03_182522) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_29_153050) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "featured_image"
+    t.string "description"
   end
 
   create_table "aircraft_images", force: :cascade do |t|
