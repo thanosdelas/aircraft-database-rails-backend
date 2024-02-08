60a4a93ed2b5b0b6cbc8aea786f3414d96b7c334
delasthanos <delasthanos@gmail.com>
2024-02-08 03:13:58 +0200

    Fetch aircraft by id API endpoint

--- app/api/aircraft_api.rb
+++ app/api/aircraft_api.rb
@@ -3,18 +3,34 @@
 class AircraftAPI < Grape::API
   resource :aircraft do
     get do
-      aircraft = JSON.parse(
-        ::Aircraft.includes(:images).where(wikipedia_info_collected: true)
-      .to_json(include: :images))
+      aircraft = ::Aircraft.select(:id, :model, :wikipedia_title, :featured_image)
+                           .where(wikipedia_info_collected: true)
 
       http_code = 200
       data = {
         status: 'ok',
-        message: 'Sucessfully fetched images',
+        message: 'Sucessfully fetched data',
         data: aircraft
       }
 
       render_response(http_code: http_code, data: data)
+    end
+
+    route_param :id do
+      get do
+        aircraft = JSON.parse(
+          ::Aircraft.includes(:images).find_by(id: params[:id])
+        .to_json(include: :images))
+
+        http_code = 200
+        data = {
+          status: 'ok',
+          message: 'Sucessfully fetched data',
+          data: aircraft
+        }
+
+        render_response(http_code: http_code, data: data)
+      end
     end
   end
 end
